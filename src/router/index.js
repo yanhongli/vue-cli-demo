7cc9a9bc8e755ccd22e043d8cbbf4a6080fb923b
李彦鸿 <liyanhong@corp.netease.com>
2020-04-20 18:55:36 +0800

    views init

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Home from '../views/home.vue'
 Vue.use(VueRouter)
 export const router = new VueRouter({
-	routes: []
+	routes: [
+		{
+			path: '/home',
+			component: Home
+		},
+		{
+			path: '**',
+			component: Home
+		}
+	]
 })
