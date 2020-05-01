bdd54805b601ea53030a47dff13e454d803ce503
yanhongli <1559652668@qq.com>
2020-05-02 01:02:29 +0800

    header init

--- src/Layout.vue
+++ src/Layout.vue
@@ -1,20 +1,35 @@
 <template>
   <el-container>
-    <el-header>
-      <el-row type='flex' justify='space-between' style="height: 100%">
-        <div>
-          image
-        </div>
-        <el-col>
-          <img :src='userInfo.avatar' class="resposive-img"/>
-        </el-col>
+    <el-header style="border-bottom: 1px solid #dbdbdb;">
+      <el-row type="flex" justify="space-between" style="height: 100%">
+        <el-row type="flex" align="middle">
+          <span class="logo"></span>
+        </el-row>
+        <el-row type="flex" align="middle">
+          <el-input place-holder="请输入搜索内容" class="mr-10" suffix-icon="el-icon-search"></el-input>
+          <el-dropdown>
+            <el-row type="flex" align="middle" style="width: 80px;">
+              <img :src="userInfo.avatar" class="resposive-img mr-10" height="30" />
+              <a>{{userInfo.name}}</a>
+            </el-row>
+
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>
+                <router-link to="/">设置</router-link>
+              </el-dropdown-item>
+              <el-dropdown-item>
+                <router-link to="/">退出</router-link>
+              </el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-row>
       </el-row>
     </el-header>
     <el-container>
       <el-aside>
         <el-menu>
           <template v-for="(menu, index) in menus">
-            <template v-if='menu.children && menu.children.length'>
+            <template v-if="menu.children && menu.children.length">
               <el-submenu :index="menu.id | toString" :key="menu.id">
                 <template slot="title">
                   <i :class="[icons[index]]"></i>
@@ -34,7 +49,7 @@
             <template v-else>
               <el-menu-item :key="menu.id">
                 <i :class="[icons[index]]"></i>
-                  <span slot="title">{{menu.name}}</span>
+                <span slot="title">{{menu.name}}</span>
               </el-menu-item>
             </template>
           </template>
@@ -53,15 +68,21 @@ import { mapGetters } from "vuex";
 export default {
   data() {
     return {
-      icons: ['el-icon-location','el-icon-menu','el-icon-document','el-icon-setting','el-icon-s-custom','el-icon-tickets','el-icon-document-checked']
-    }
+      icons: [
+        "el-icon-location",
+        "el-icon-menu",
+        "el-icon-document",
+        "el-icon-setting",
+        "el-icon-s-custom",
+        "el-icon-tickets",
+        "el-icon-document-checked"
+      ]
+    };
   },
   computed: {
     ...mapGetters({ menus: "GET_ROUTES", userInfo: "GET_USERINFO" })
   },
-  mounted() {
-    
-  }
+  mounted() {}
 };
 </script>
 
